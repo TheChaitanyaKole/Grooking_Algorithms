3317e3a3cacce75118d52a6b9b413535e320432a
Chaitanya Kole <chaitanyakole07@gmail.com>
2022-07-08 00:01:16 +0530

    Binary Search Inplementation

---
+++ Binary_search.py
@@ -0,0 +1,31 @@
+from random import randint
+
+def binary_search(list,to_search):
+    low = 0
+    high = len(list) - 1
+    mid = 0
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        if list[mid] < to_search:
+            low = mid + 1
+        elif list[mid] > to_search:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+
+
+if __name__ == '__main__':
+    numbers = int(input("Enter Array Size : "))
+    list = []
+    for i in range(numbers):
+        list.append(randint(0,10))
+    list.sort()
+    for i in list:
+        print(i,end="   ")
+    print()
+    to_search = int(input("Enter Number to Search : "))
+    result = binary_search(list,to_search)
+    print(to_search , " fount at position : " , result)
