e17ffeee4c777ba760d7df1650a0b5e967c4815a
Chaitanya Kole <chaitanyakole07@gmail.com>
2022-07-08 13:13:06 +0530

    Binary Search in CPP

---
+++ Binary_Search.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int arr[], int to_search, int size)
+{
+  int high,low,mid;
+  low = 0;
+  high = size;
+  while (low <= high)
+  {
+    mid = (high+low)/2;
+    if (arr[mid] < to_search)
+    {
+      low = mid+1;
+    }
+    else if (arr[mid] > to_search)
+    {
+      high = mid-1;
+    }
+    else
+      return mid;
+  }
+  return -1;
+}
+
+int main(int argc, char const *argv[]) {
+  int n;
+  cout << "Enter Number Of Elements : ";
+  cin >> n;
+  int arr[n];
+  srand(time(0));
+  for (int i = 0; i < n; i++)
+  {
+    arr[i] = rand()%100;
+  }
+  sort(arr,arr+n);
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  cout << "\nEnter Element To Search : ";
+  int to_search;
+  cin >> to_search;
+  int pos;
+  pos = binarySearch(arr, to_search, n);
+  cout << to_search << " found at position : " << pos << endl;
+  return 0;
+}
