c7e0ab292b50dbab1ebee0da9c3eb0f286640147
Chaitanya Kole <chaitanyakole07@gmail.com>
2022-07-09 18:34:25 +0530

    Sum of elements in array using recursion

---
+++ SumOfArray.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum(int arr[], int size)
+{
+  if (size == 0)
+    return arr[size];
+  else
+    return arr[size] + sum(arr, size-1);
+}
+
+int main(int argc, char const *argv[]) {
+  int size;
+  cin >> size;
+  int arr[size];
+  srand(time(0));
+  for(int i=0; i<size; i++)
+    arr[i] = rand()%100;
+  for(int i=0; i<size; i++)
+    cout << arr[i] << " ";
+  cout << endl;
+  cout << "\nSum of Array is : " << sum(arr, size-1) << endl;
+  return 0;
+}
