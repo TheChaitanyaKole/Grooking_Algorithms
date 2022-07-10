cc99754d7db8b6942981989936e37ab55e36393a
Chaitanya Kole <chaitanyakole07@gmail.com>
2022-07-10 13:48:12 +0530

    To find number of items in a list using recursion

---
+++ NoOfItemsInAListUsingRecusrion.py
@@ -0,0 +1,19 @@
+from random import randint
+import time
+
+def length(list):
+    if list:
+        return 1 + length(list[1:])
+    else:
+        return 0
+
+if __name__ == "__main__":
+    list = []
+    for i in range(randint(0,15)):
+        list.append(randint(0,100))
+    for i in list:
+        print(i,end=" ",flush=True)
+        time.sleep(0.1)
+    print()
+    leng = length(list)
+    print("length of list is ",leng)
